72cb0da61cecf3ce0d97946285631370969e616a
Anton Buzynnikov <a.buzynnikov@mail.ru>
2025-05-30 19:46:46 +0600

    complete

---
+++ StreamGenerateDigit/src/main/java/ru/buzynnikov/StreamCollectorsExample.java
@@ -0,0 +1,44 @@
+package ru.buzynnikov;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamCollectorsExample {
+    public static void main(String[] args) {
+        List<Order> orders = List.of(
+                new Order("Laptop", 1200.0),
+                new Order("Smartphone", 800.0),
+                new Order("Laptop", 1500.0),
+                new Order("Tablet", 500.0),
+                new Order("Smartphone", 900.0)
+        );
+
+
+        orders.stream()
+                .collect(Collectors.groupingBy(Order::getProduct, Collectors.summingDouble(Order::getCost)))
+                .entrySet()
+                .stream()
+                .sorted((a,b)->b.getValue().compareTo(a.getValue()))
+                .limit(3)
+                .forEach(System.out::println);
+
+    }
+}
+class Order {
+    private String product;
+    private double cost;
+
+    public Order(String product, double cost) {
+        this.product = product;
+        this.cost = cost;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
